57584911b5a071ff1649cae959510464be9d8256
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-06 14:09:07 -0500

    Section 3 - Create Category Controller

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers;
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
