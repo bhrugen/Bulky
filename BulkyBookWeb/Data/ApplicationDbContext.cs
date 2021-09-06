6516488ce531064bd7bae8052217b6a61ff19463
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-06 13:42:06 -0500

    Section 2 - Add ApplicationDbContext

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data;
+public class ApplicationDbContext :DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories {  get; set; }
+}
