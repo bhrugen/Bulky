fbe2f8dc1b8d50cdaf72f87fe04d14527fea9e7c
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-06 13:50:05 -0500

    Section 2 - Setup Program.cs to use DbContext

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,8 +1,14 @@
 
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));  
 
 var app = builder.Build();
 
