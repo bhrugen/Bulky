d821d9470a406224cd7e3b121e4e62349c9e99d2
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-06 13:18:31 -0500

    Section 2 - Create Category Model

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BulkyBookWeb.Models;
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+}
