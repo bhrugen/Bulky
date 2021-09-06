e6c7b81384c835e882eb04d8f146ac680131e080
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-06 14:45:55 -0500

    Section 3 - Bootswatch Theme

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -8,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
-    ));  
-
+    ));
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
