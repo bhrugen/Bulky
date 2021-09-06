60883909f2d6456d23fed5bae72d2528a6bcc95d
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-06 14:18:37 -0500

    Section 3 - Retrieve all Categories

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers;
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
     public IActionResult Index()
     {
+        var objCategoryList = _db.Categories.ToList();
         return View();
     }
 }
