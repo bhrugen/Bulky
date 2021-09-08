d206e1eac499e4c9d24045db949d88763ba0e068
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-08 11:27:03 -0500

    Section 3 - Display Name and Range Validation

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models;
@@ -8,6 +9,8 @@ public class Category
     public int Id { get; set; }
     [Required]
     public string Name { get; set; }
+    [DisplayName("Display Order")]
+    [Range(1,100,ErrorMessage ="Display Order must be between 1 and 100 only!!")]
     public int DisplayOrder { get; set; }
     public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 }
