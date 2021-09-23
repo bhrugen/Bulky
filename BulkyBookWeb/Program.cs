2b4a201d9a83e8d61faaa434ee3e8e903181595a
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-23 13:52:13 -0500

    Section 5 - Areas in .NET Core

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -40,6 +40,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
