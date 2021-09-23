13fe8dbeaca6ae9bfdc32f07d38ca2b10ca940e9
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-23 17:19:27 -0500

    Section 6 - Loading Datatables

---
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -0,0 +1,19 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url":"/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "price", "width": "15%" },
+            { "data": "author", "width": "15%" },
+        ]
+    });
+}
