3de130fe7aa13ee3f83e01fc04b0e737e4877dfd
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-23 16:39:19 -0500

    Section 6 - Sweetalerts

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
@@ -61,12 +62,12 @@ public class ProductController : Controller
     //POST
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Upsert(CoverType obj)
+    public IActionResult Upsert(ProductVM obj, IFormFile file)
     {
        
         if (ModelState.IsValid)
         {
-            _unitOfWork.CoverType.Update(obj);
+            //_unitOfWork.CoverType.Update(obj);
             _unitOfWork.Save();
             TempData["success"] = "CoverType updated successfully";
             return RedirectToAction("Index");
