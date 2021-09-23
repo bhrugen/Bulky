fceac0f3090d4c28ae2a2dff0569ae3ef1b95c26
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-23 11:31:54 -0500

    Section 5 - Implement Category Repository

---
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
