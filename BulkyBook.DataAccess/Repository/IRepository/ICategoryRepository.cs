4d7c9436a71233922e0393c3c1a0544b1046d825
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-23 11:23:29 -0500

    Section 5 - Category Repository Interface

---
+++ BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
