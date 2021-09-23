c911d798b125a8fd44cae04df7e279aea9ec56bf
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-23 17:39:21 -0500

    SEction 6 - Render Html in datatables

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -15,6 +15,20 @@ function loadDataTable() {
             { "data": "price", "width": "15%" },
             { "data": "author", "width": "15%" },
             { "data": "category.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                        <a href="/Admin/Product/Upsert?id=${data}"
+                        class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                        <a 
+                        class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+					</div>
+                        `
+                },
+                "width": "15%"
+            }
         ]
     });
 }
