7625af44da56da3d6155b8041ba1acf779e62464
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-25 11:24:45 -0500

    Section 9 - Configure Application Cookie

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -23,6 +23,13 @@ builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddDefaultTokenProvide
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
