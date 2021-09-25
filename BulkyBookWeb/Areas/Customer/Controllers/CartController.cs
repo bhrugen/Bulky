a67134296d56642e5779c8e0a37cf5cc8fc5f5d8
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-25 11:52:26 -0500

    Section 9 - Shopping Cart UI

---
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
