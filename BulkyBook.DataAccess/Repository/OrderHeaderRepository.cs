0cf468005f60c837f338471d94fd60ec1e05a3c8
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-26 16:34:04 -0500

    Section 11 - Loading Order Details Header

--- BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
+++ BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
@@ -39,7 +39,7 @@ namespace BulkyBook.DataAccess.Repository
         public void UpdateStripePaymentID(int id, string sessionId, string paymentItentId)
         {
             var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
-
+            orderFromDb.PaymentDate = DateTime.Now;
             orderFromDb.SessionId = sessionId;
             orderFromDb.PaymentIntentId = paymentItentId;
         }
