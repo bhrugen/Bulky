9565c1bb09810c7a29230ea52e4a326b172901f6
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-27 14:16:09 -0500

    Section 12 - SendGrid

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,6 +1,9 @@
 ﻿using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
 using MimeKit;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,24 +14,37 @@ namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var emailToSend = new MimeMessage();
-            emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
-            emailToSend.To.Add(MailboxAddress.Parse(email));
-            emailToSend.Subject = subject;
-            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text = htmlMessage};
+            //var emailToSend = new MimeMessage();
+            //emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
+            //emailToSend.To.Add(MailboxAddress.Parse(email));
+            //emailToSend.Subject = subject;
+            //emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text = htmlMessage};
 
-            //send email
-            using (var emailClient = new SmtpClient())
-            {
-                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-                emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$");
-                emailClient.Send(emailToSend);
-                emailClient.Disconnect(true);
-            }
+            ////send email
+            //using (var emailClient = new SmtpClient())
+            //{
+            //    emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+            //    emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$");
+            //    emailClient.Send(emailToSend);
+            //    emailClient.Disconnect(true);
+            //}
 
-            return Task.CompletedTask;
+            //return Task.CompletedTask;
+
+            var client = new SendGridClient(SendGridSecret);
+            var from = new EmailAddress("hello@dotnetmastery.com", "Bulky Book");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject,"", htmlMessage);
+            return client.SendEmailAsync(msg);
 
         }
     }
