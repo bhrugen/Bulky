4995e7a999e57b0c3d75cc32c307e17d688fd415
Pratham Savjani <87868351+PrathamSavjani@users.noreply.github.com>
2023-04-18 01:09:01 +0530

    Update order.js
    Dynamic status passing as parameter in case of adding multiple status in future.

--- BulkyBookWeb/wwwroot/js/order.js
+++ BulkyBookWeb/wwwroot/js/order.js
@@ -1,28 +1,9 @@
 ﻿var dataTable;
 
 $(document).ready(function () {
-    var url = window.location.search;
-    if (url.includes("inprocess")) {
-        loadDataTable("inprocess");
-    }
-    else {
-        if (url.includes("completed")) {
-            loadDataTable("completed");
-        }
-        else {
-            if (url.includes("pending")) {
-                loadDataTable("pending");
-            }
-            else {
-                if (url.includes("approved")) {
-                    loadDataTable("approved");
-                }
-                else {
-                    loadDataTable("all");
-                }
-            }
-        }
-    }
+    var urlParams = new URLSearchParams(window.location.search);
+    var status = urlParams.get('status');
+    loadDataTable(status);
 });
 
 function loadDataTable(status) {
